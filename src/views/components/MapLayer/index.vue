eaff068c3e4154581b4e1e15a220ddf6af1a7a2f
王成 <1236192@qq.com>
2022-08-23 09:35:04 +0800

    :fire:

---
+++ src/views/components/MapLayer/index.vue
@@ -0,0 +1,19 @@
+<!--------------------------------------------
+ ¦ 文件名
+ ¦ 文件描述
+ ¦
+ ¦ Author: 大风
+ ¦ Date: 2022-08-23
+ ¦------------------------------------------->
+
+<template>
+  <div>vue template</div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentName'
+}
+</script>
+
+<style scoped></style>
