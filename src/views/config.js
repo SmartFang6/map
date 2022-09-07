c7f74f9fbaef9f444ca5c220e31ef60bdb7f8c27
wanglin <wanglin@0594@163.com>
2022-09-07 08:46:53 +0800

    feat:config.js新增ADCD

--- src/views/config.js
+++ src/views/config.js
@@ -10,21 +10,29 @@ export const NoticeEvt = {
 export const ticketMap = [
   {
     name: "苍南县",
+    adcd: "330327",
     ticket: "52AC28D30148995B8F34B0C0209CD63B",
   },
   {
     name: "吴兴区",
+    adcd: "330502",
+
     ticket: "5506EE3FF57CB116438CB87D74A2D8AE",
   },
   {
     name: "定海区",
+    adcd: "330902",
+
     ticket: "7026F336D8F42AAE1887156908030CB7",
   },
   {
     name: "建德市",
+    adcd: "330182",
+
     ticket: "FC37BAB8805D85AF2576563A20F66658",
   },
   {
+    adcd: "330782",
     name: "义乌市",
     ticket: "5719416B1AF12B2653A222FAF5792252",
   },
