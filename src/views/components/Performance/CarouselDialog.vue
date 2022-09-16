a9d5c551abdbe4558b48382c7963ce5714008e3b
王卫国 <weiguozh.wang@outlook.com>
2022-09-16 11:21:09 +0800

    取消pdf文件的注释

--- src/views/components/Performance/CarouselDialog.vue
+++ src/views/components/Performance/CarouselDialog.vue
@@ -23,7 +23,7 @@
       <div class="pdf-preview">
         <vue-pdf-embed
           class="vue-pdf-embed"
-          v-if="!activeFiles?.source"
+          v-if="activeFiles?.source"
           :source="activeFiles?.source"
           :page="activeFiles?.pageNum"
           :style="scaleFun"
