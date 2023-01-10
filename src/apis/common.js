3828f7b70976d459771667e67de691cfebe82d5a
杨清方 <1270358909@qq.com>
2023-01-10 10:00:47 +0800

    feat 图层数量接口更换到驾驶舱接口

--- src/apis/common.js
+++ src/apis/common.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: bifang
+ * @Date: 2022-11-23 18:28:45
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2023-01-10 09:59:20
+ * @FilePath: /river-lake-cockpit-front/src/apis/common.js
+ */
 import { getHttp, postHttp } from "@/utils/axios";
 /**
  * 根据字典的表和列代码组合查询字典集合
@@ -14,11 +21,5 @@ export const getCustomTicket = (data) =>
  * 查看水域数量统计信息
  */
 export const getSurvreyTypeStat = (data) => {
-  return postHttp(
-    "/survreyCommonBiz/getSurvreyTypeStat",
-    { data },
-    {
-      baseURL: "/onemap-api",
-    }
-  );
+  return postHttp("/supervision/getSurvreyTypeStat", { data });
 };
