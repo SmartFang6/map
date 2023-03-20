e7a6c63677534b7b48971b667d3e24f417aac3c3
杨清方 <1270358909@qq.com>
2023-03-20 18:44:56 +0800

    feat 下拉框高度问题解决

--- src/App.vue
+++ src/App.vue
@@ -125,4 +125,26 @@ a {
     background: rgba(255, 255, 255, 0.15);
   }
 }
+.el-dropdown-menu {
+  max-height: 50vh;
+  overflow: hidden;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    /*滚动条整体样式*/
+    width: 10px; /*高宽分别对应横竖滚动条的尺寸*/
+    height: 1px;
+  }
+  &::-webkit-scrollbar-thumb {
+    /*滚动条里面小方块*/
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    background: rgba(243, 243, 243, 0.75);
+  }
+  &::-webkit-scrollbar-track {
+    /*滚动条里面轨道*/
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    background: rgba(255, 255, 255, 0.15);
+  }
+}
 </style>
